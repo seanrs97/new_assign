372f5864f7f5060c6ae87a18e7a3a5d454e6b8af
sean stevenson <sean stevenson>
2017-04-30 00:46:43 +0100

    new stuff

--- serviceworker.js
+++ serviceworker.js
@@ -109,7 +109,7 @@ var CACHED_URLS = [
     BASE_PATH + 'styles.css'
 ];
 
-var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBMA8ic4_Gyh0D7XUW9gBtrCcEwoBbBJ8E&libraries=places&callback=initMap'
+var googleMapsAPIJS = ="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMA8ic4_Gyh0D7XUW9gBtrCcEwoBbBJ8E&libraries=places&callback=initMap"
 
 self.addEventListener('install', function(event) {
     // Cache everything in CACHED_URLS. Installation fails if anything fails to cache
